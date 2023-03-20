c4382bab010d9fd783a6c6af615e365ffab5747a
BladNunez <Nunez.Bladimir@yahoo.com>
2023-03-19 22:40:39 -0400

    create book constructor

---
+++ library.js
@@ -0,0 +1,8 @@
+// create book constructor
+function Book(name,title,pages,read){
+        this.name = name;
+        this.title = title;
+        this.pages = pages;
+        this.read = read;
+}
+
