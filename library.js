f9319747ac207b9aea7e4d75f8c042dd9c83184f
BladNunez <Nunez.Bladimir@yahoo.com>
2023-03-19 22:59:03 -0400

    prompt user input for information on book and push onto array

--- library.js
+++ library.js
@@ -1,6 +1,6 @@
 // create book constructor
-function Book(name,title,pages,read){
-        this.name = name;
+function Book(title,author,pages,read){
+        this.author = author;
         this.title = title;
         this.pages = pages;
         this.read = read;
@@ -10,5 +10,13 @@ function Book(name,title,pages,read){
 let myLibrary = [];
 
 function addBookToLibrary(){
+        const title = prompt("Title of book?:");
+        const author = prompt("Author of book?:");
+        const pages = Number(prompt("Amount of pages?:"));
+        const read = prompt("Have you read book?:");
+
+        const bookOne = new Book(title,author,pages,read);
+
+        myLibrary.push(bookOne);
         
 }
