a3f3d022e7a2864b06a6de6760f1d8799ea2d3d3
BladNunez <Nunez.Bladimir@yahoo.com>
2023-03-23 20:21:44 -0400

    add preventDefault on form when button pressed

--- library.js
+++ library.js
@@ -45,12 +45,24 @@ function displayBooks(){
 
 const button = document.querySelector('button');
 const container = document.getElementById('#content-container');
-const form = document.getElementById("#bookForm");
 
-button.addEventListener('click' , function() {
+button.addEventListener('click' , function(e) {
         document.getElementById("bookForm").style.display = "block";
-
 });
+
+const submit = document.getElementById('submitButton');
+
+submit.addEventListener('click',function(e){
+
+        let bookInfo = new Book(document.getElementById('title').value,
+                                document.getElementById('author').value,
+                                document.getElementById('pages').value,
+                                document.getElementById('read').value);
+                                      
+        //let myObj = JSON.stringify(bookInfo);
+        e.preventDefault();  
+});
+
 
 window.onload = function() {
         document.getElementById("bookForm").style.display = "none";
